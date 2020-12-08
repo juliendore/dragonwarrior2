9fd344d67d8d808f67974d3227b893fd068d7d74
Julien DORE <julien.dore@le-campus-numerique.fr>
2020-12-08 09:50:36 +0100

    Created /characters POST route to create a new character

--- src/main/java/com/javaadvanced/dragonwarrior/web/controller/CharacterController.java
+++ src/main/java/com/javaadvanced/dragonwarrior/web/controller/CharacterController.java
@@ -2,9 +2,12 @@ package com.javaadvanced.dragonwarrior.web.controller;
 
 import com.javaadvanced.dragonwarrior.dao.CharacterDao;
 import com.javaadvanced.dragonwarrior.model.Character;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -22,5 +25,30 @@ public class CharacterController {
     @GetMapping(value = "/characters/{id}")
     public Character getOneCharacter(@PathVariable int id) {
         return characterDao.findById(id);
+    }
+
+//    @PostMapping(value = "/characters")
+//    public void addOneCharacter(@RequestBody Character character){
+//        characterDao.save(character);
+//    }
+
+    //    ajouter un personnage
+//    ResponseEntitty : permet de définir le code http retourné
+    @PostMapping(value = "/characters")
+    public ResponseEntity<Void> addOneCharacter(@RequestBody Character character) {
+
+        Character characterAdded = characterDao.save(character);
+
+        if (characterAdded == null)
+//            204 No Content + header
+            return ResponseEntity.noContent().build();
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(characterAdded.getId())
+                .toUri();
+//        Header built with characterAdded location
+        return ResponseEntity.created(location).build();
     }
 }
